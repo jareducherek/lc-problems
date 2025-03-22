4b2a1d1f4ff52d3544b4eb76f05b3e6b0c30a608
Collin Trovada <trovadacollin@gmail.com>
2025-03-22 13:38:06 -0500

    Add Python Utils

---
+++ Utils/helpers.py
@@ -0,0 +1,13 @@
+def array_to_linked_list(arr):
+    dummy = ListNode()
+    current = dummy
+    for val in arr:
+        current.next = ListNode(val)
+        current = current.next
+    return dummy.nextdef array_to_linked_list(arr):
+    dummy = ListNode()
+    current = dummy
+    for val in arr:
+        current.next = ListNode(val)
+        current = current.next
+    return dummy.next
