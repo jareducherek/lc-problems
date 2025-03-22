38d60fc0cc71653c0efeabc602d185d7bed7732b
Collin Trovada <trovadacollin@gmail.com>
2025-03-22 14:13:56 -0500

    Fix helper and do 3/22 daily

---
+++ utils/helpers.py
@@ -0,0 +1,7 @@
+def array_to_linked_list(arr):
+    dummy = ListNode()
+    current = dummy
+    for val in arr:
+        current.next = ListNode(val)
+        current = current.next
+    return dummy.next
